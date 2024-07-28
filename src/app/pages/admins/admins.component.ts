afee60909babff1c3578b2eb4d8749063f4bf1a9
ws-vipul <workspace_vipul@outlook.com>
2024-07-28 20:24:13 +0530

    update admin tab for modals and backend integration

--- src/app/pages/admins/admins.component.ts
+++ src/app/pages/admins/admins.component.ts
@@ -1,12 +1,15 @@
 import { NgFor } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-admins',
   standalone: true,
   imports: [
-    HttpClientModule, NgFor
+    HttpClientModule, NgFor, ReactiveFormsModule
   ],
   templateUrl: './admins.component.html',
   styleUrl: './admins.component.css'
@@ -15,17 +18,37 @@ import { Component, OnInit } from '@angular/core';
 export class AdminsComponent implements OnInit{
 
   private getJsonValue : any;
-  private postJsonValue : any;
+  
   private responsePayload: any;
   public allAdmins: any;
+  public adminLogs: any;
 
+  postAdminForm : any;
+  postEditAdminForm : any;
+  adminDetails: any;
 
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient, 
+    private formBuilder: FormBuilder) 
+    { }
 
   ngOnInit(): void {
     this.fetchAllAdminsList();
+
+    this.postAdminForm = this.formBuilder.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      mobNum: '',
+      password: '',
+      adminId: '1000'
+    })
+
+    this.postEditAdminForm = this.formBuilder.group({
+      editFirstName: '',
+      editLastName: '',
+      editEmail: '',
+      editMobNum: '',
+    })
   }
 
   OnAdminsFetch() {
@@ -42,11 +65,91 @@ export class AdminsComponent implements OnInit{
         if (this.getJsonValue.status === "success") {
             this.responsePayload = this.getJsonValue.payload
             this.allAdmins = this.responsePayload 
-            console.log(this.allAdmins)
         }
 
     })
 
   }
+
+  crateAdmin() {
+    const postAdmin = this.postAdminForm.value
+    this.http.post('/dashboard/register/admin', postAdmin).subscribe((response) => {
+        this.getJsonValue = response
+
+        if (this.getJsonValue.status === "success") {
+          window.location.reload()
+        }
+      }
+    );
+  }
+
+  fetchAdminDetails(id : number) {
+
+
+    this.http.get('/dashboard/get/admin/details/' + id)
+    .subscribe((response) => {
+
+        this.getJsonValue = response
+
+        if (this.getJsonValue.status === "success") {
+            console.log(response);
+            let payload = this.getJsonValue.payload
+            this.postEditAdminForm = this.formBuilder.group({
+              editFirstName: payload.firstName,
+              editLastName: payload.lastName,
+              editEmail: payload.email,
+              editMobNum: payload.mobNum,
+            })
+        }
+
+    })
+
+  }
+
+  fetchAdminLogs(id: number) {
+    this.http.get('/dashboard/get/admin/activity/logs/' + id)
+    .subscribe((response) => {
+
+        this.getJsonValue = response
+
+        if (this.getJsonValue.status === "success") {
+            console.log(response);
+            this.adminLogs = this.getJsonValue.payload
+            
+        }
+
+    })
+  }
+
+  editAdmin() {
+    const postEditAdmin = this.postEditAdminForm.value;
+    this.http.post('/dashboard/register/admin', postEditAdmin).subscribe((response) => {
+        this.getJsonValue = response
+
+        if (this.getJsonValue.status === "success") {
+          window.location.reload()
+        }
+      }
+    );
+  }
+
+  deleteAdmin(id : number) {
+
+    console.log('printing :' + id)
+    let deleteAdminForm = {
+      userAdminId : id,
+      reason: 'Testing',
+      adminId: 1000
+    }
+
+    this.http.put('dashboard/delete/admin', deleteAdminForm).subscribe((response) => {
+      this.getJsonValue = response
+
+        if (this.getJsonValue.status === "success") {
+          window.location.reload()
+        }
+    })
+  }
+
 
 }
