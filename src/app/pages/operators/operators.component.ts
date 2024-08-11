b4e6d8761e0815859921a869d18ce730b4e94757
ws-vipul <workspace_vipul@outlook.com>
2024-08-11 15:19:47 +0530

    updated ui for export data

--- src/app/pages/operators/operators.component.ts
+++ src/app/pages/operators/operators.component.ts
@@ -1,11 +1,12 @@
 import { NgFor } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-operators',
   standalone: true,
-  imports: [HttpClientModule, NgFor],
+  imports: [HttpClientModule, NgFor, ReactiveFormsModule],
   templateUrl: './operators.component.html',
   styleUrl: './operators.component.css'
 })
@@ -15,15 +16,37 @@ export class OperatorsComponent {
   private responsePayload: any;
   public allOperators: any;
 
-  constructor(private http: HttpClient) {
+  public operatorLogs: any;
+
+  postOperatorForm : any;
+  postEditOperatorForm : any;
+  operatorDetails: any;
+
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
 
   }
 
   ngOnInit(): void {
     this.fetchAllOperatorsList();
+
+    this.postOperatorForm = this.formBuilder.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      mobNum: '',
+      password: '',
+      operatorId: '1000'
+    })
+
+    this.postEditOperatorForm = this.formBuilder.group({
+      editFirstName: '',
+      editLastName: '',
+      editEmail: '',
+      editMobNum: '',
+    })
   }
 
-  OnAdminsFetch() {
+  OnOperatorsFetch() {
     this.fetchAllOperatorsList();
   }
 
@@ -42,6 +65,87 @@ export class OperatorsComponent {
 
     })
 
+  }
+
+
+  crateOperator() {
+    const postOperator = this.postOperatorForm.value
+    this.http.post('/dashboard/register/operator', postOperator).subscribe((response) => {
+        this.getJsonValue = response
+
+        if (this.getJsonValue.status === "success") {
+          window.location.reload()
+        }
+      }
+    );
+  }
+
+  fetchOperatorDetails(id : number) {
+
+
+    this.http.get('/dashboard/get/operator/details/' + id)
+    .subscribe((response) => {
+
+        this.getJsonValue = response
+
+        if (this.getJsonValue.status === "success") {
+            console.log(response);
+            let payload = this.getJsonValue.payload
+            this.postEditOperatorForm = this.formBuilder.group({
+              editFirstName: payload.firstName,
+              editLastName: payload.lastName,
+              editEmail: payload.email,
+              editMobNum: payload.mobNum,
+            })
+        }
+
+    })
+
+  }
+
+  fetchOperatorLogs(id: number) {
+    this.http.get('/dashboard/get/operator/activity/logs/' + id)
+    .subscribe((response) => {
+
+        this.getJsonValue = response
+
+        if (this.getJsonValue.status === "success") {
+            console.log(response);
+            this.operatorLogs = this.getJsonValue.payload
+            
+        }
+
+    })
+  }
+
+  editOperator() {
+    const postEditOperator = this.postEditOperatorForm.value;
+    this.http.post('/dashboard/register/operator', postEditOperator).subscribe((response) => {
+        this.getJsonValue = response
+
+        if (this.getJsonValue.status === "success") {
+          window.location.reload()
+        }
+      }
+    );
+  }
+
+  deleteOperator(id : number) {
+
+    console.log('printing :' + id)
+    let deleteOperatorForm = {
+      userOperatorId : id,
+      reason: 'Testing',
+      operatorId: 1000
+    }
+
+    this.http.put('dashboard/delete/operator', deleteOperatorForm).subscribe((response) => {
+      this.getJsonValue = response
+
+        if (this.getJsonValue.status === "success") {
+          window.location.reload()
+        }
+    })
   }
 
 }
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,11 +1,12 @@
 import { NgFor } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [HttpClientModule, NgFor],
+  imports: [HttpClientModule, NgFor, ReactiveFormsModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
@@ -17,13 +18,33 @@ export class UsersComponent {
   private responsePayload: any;
   public allUsers: any;
 
+  public userLogs: any;
+  postUserForm : any;
+  postEditUserForm : any;
+  userDetails: any;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
 
   }
 
   ngOnInit(): void {
     this.fetchAllUsersList();
+
+    this.postUserForm = this.formBuilder.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      mobNum: '',
+      password: '',
+      userId: '1000'
+    })
+
+    this.postEditUserForm = this.formBuilder.group({
+      editFirstName: '',
+      editLastName: '',
+      editEmail: '',
+      editMobNum: '',
+    })
   }
 
   OnUsersFetch() {
@@ -45,6 +66,86 @@ export class UsersComponent {
 
     })
 
+  }
+
+  crateUser() {
+    const postUser = this.postUserForm.value
+    this.http.post('/dashboard/register/user', postUser).subscribe((response) => {
+        this.getJsonValue = response
+
+        if (this.getJsonValue.status === "success") {
+          window.location.reload()
+        }
+      }
+    );
+  }
+
+  fetchUserDetails(id : number) {
+
+
+    this.http.get('/dashboard/get/user/details/' + id)
+    .subscribe((response) => {
+
+        this.getJsonValue = response
+
+        if (this.getJsonValue.status === "success") {
+            console.log(response);
+            let payload = this.getJsonValue.payload
+            this.postEditUserForm = this.formBuilder.group({
+              editFirstName: payload.firstName,
+              editLastName: payload.lastName,
+              editEmail: payload.email,
+              editMobNum: payload.mobNum,
+            })
+        }
+
+    })
+
+  }
+
+  fetchUserLogs(id: number) {
+    this.http.get('/dashboard/get/user/activity/logs/' + id)
+    .subscribe((response) => {
+
+        this.getJsonValue = response
+
+        if (this.getJsonValue.status === "success") {
+            console.log(response);
+            this.userLogs = this.getJsonValue.payload
+            
+        }
+
+    })
+  }
+
+  editUser() {
+    const postEditUser = this.postEditUserForm.value;
+    this.http.post('/dashboard/register/user', postEditUser).subscribe((response) => {
+        this.getJsonValue = response
+
+        if (this.getJsonValue.status === "success") {
+          window.location.reload()
+        }
+      }
+    );
+  }
+
+  deleteUser(id : number) {
+
+    console.log('printing :' + id)
+    let deleteUserForm = {
+      userUserId : id,
+      reason: 'Testing',
+      userId: 1000
+    }
+
+    this.http.put('dashboard/delete/user', deleteUserForm).subscribe((response) => {
+      this.getJsonValue = response
+
+        if (this.getJsonValue.status === "success") {
+          window.location.reload()
+        }
+    })
   }
 
 }
